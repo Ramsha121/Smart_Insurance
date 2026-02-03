75e2abe5396fdae8badcb09b8a8e4f42d976e523
Data Scientist <89708093+Ramsha121@users.noreply.github.com>
2026-02-03 16:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("""
     <style>
     .main { background-color: #2996c4; }
     .stMetric { background-color: #ffffff; padding: 15px; border-radius: 10px; box-shadow: 0 2px 4px rgba(0,0,0,0.05); }
-    .insight-box { background-color: #174d48; padding: 20px; border-radius: 10px; border-left: 5px solid #2196f3; margin-bottom: 20px; }
+    .insight-box { background-color: #b8624d; padding: 20px; border-radius: 10px; border-left: 5px solid #2196f3; margin-bottom: 20px; }
     h1, h2, h3 { color: #2c3e50; }
     </style>
 """, unsafe_allow_html=True) # ✅ Fixed!
